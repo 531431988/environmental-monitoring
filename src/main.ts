99eb4577c191fe2cff8881fe516fa2c93040f49b
WM <531431988@qq.com>
2024-03-24 19:02:37 +0800

    添加路由拦截

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { createApp } from "vue";
-import {
-  createRouter,
-  createWebHistory,
-} from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
+import router from './router'
 import App from "./App.vue";
 import 'uno.css'
 import './style.less'
-const router = createRouter({
-  history: createWebHistory(),
-  extendRoutes: (routes) => setupLayouts(routes),
-})
+
 const app = createApp(App)
 
 app.use(router)
---
+++ src/router.ts
@@ -0,0 +1,30 @@
+import {
+  createRouter,
+  createWebHistory,
+} from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
+import {USER_INFO} from '@/enume/cache'
+const router = createRouter({
+  history: createWebHistory(),
+  extendRoutes: (routes) => {
+    const homeRoute = routes.find((r) => r.name !== '/')
+    if (homeRoute) {
+      homeRoute.meta ??= {}
+      homeRoute.meta.requiresAuth = true
+      localStorage.getItem(USER_INFO)
+    }
+    return setupLayouts(routes)
+  },
+})
+// 设置全局前置守卫
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem(USER_INFO)
+  if (to.matched.some(record => record.meta.requiresAuth) && !token) {
+    // 如果用户未认证，则重定向到登录页面
+    next({ path: '/' });
+  } else {
+    // 否则，继续导航
+    next();
+  }
+});
+export default router
