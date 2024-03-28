4818290e38f2f510359341c4ca8672c0d56bf561
WM <531431988@qq.com>
2024-03-28 08:28:55 +0800

    添加输入框

--- src/pages/warning-config/index.vue
+++ src/pages/warning-config/index.vue
@@ -2,64 +2,62 @@
   <div class="flex flex-col h-full">
     <div class="w-80% m-auto py-24">
       <ConfigCard title="设备基本信息">
-        <div class="flex items-center justify-center">
-          <a-row :gutter="48">
-            <a-col>
-              <span class="mr-24">保持供电</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">报警指示灯</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">短信推送</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">微信推送</span><a-switch />
-            </a-col>
-          </a-row>
-        </div>
+        <a-row :gutter="48" class="flex items-center justify-center">
+          <a-col>
+            <span class="mr-24">保持供电</span><a-switch />
+          </a-col>
+          <a-col>
+            <span class="mr-24">报警指示灯</span><a-switch />
+          </a-col>
+          <a-col class="flex items-center">
+            <span class="mr-24">短信推送</span><a-switch />
+            <a-input placeholder="请输入手机号" class="w-180 ml-16" readonly value="13554291081" @click="show = true" />
+          </a-col>
+          <a-col>
+            <span class="mr-24">微信推送</span><a-switch />
+          </a-col>
+        </a-row>
       </ConfigCard>
       <ConfigCard title="RS485通信设置" class="my-32">
-        <div class="flex items-center justify-center">
-          <a-row :gutter="48">
-            <a-col>
-              <span class="mr-24">保持供电</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">报警指示灯</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">短信推送</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">微信推送</span><a-switch />
-            </a-col>
-          </a-row>
-        </div>
+        <a-row :gutter="48" class="flex items-center justify-center">
+          <a-col>
+            <span class="mr-24">保持供电</span><a-switch />
+          </a-col>
+          <a-col>
+            <span class="mr-24">报警指示灯</span><a-switch />
+          </a-col>
+          <a-col class="flex items-center">
+            <span class="mr-24">短信推送</span><a-switch />
+            <a-input placeholder="请输入手机号" class="w-180 ml-16" readonly value="13554291081" @click="show = true" />
+          </a-col>
+          <a-col>
+            <span class="mr-24">微信推送</span><a-switch />
+          </a-col>
+        </a-row>
       </ConfigCard>
       <ConfigCard title="报警阈值设置">
-        <div class="flex items-center justify-center">
-          <a-row :gutter="48">
-            <a-col>
-              <span class="mr-24">保持供电</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">报警指示灯</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">短信推送</span><a-switch />
-            </a-col>
-            <a-col>
-              <span class="mr-24">微信推送</span><a-switch />
-            </a-col>
-          </a-row>
-        </div>
+        <a-row :gutter="48" class="flex items-center justify-center">
+          <a-col>
+            <span class="mr-24">保持供电</span><a-switch />
+          </a-col>
+          <a-col>
+            <span class="mr-24">报警指示灯</span><a-switch />
+          </a-col>
+          <a-col class="flex items-center">
+            <span class="mr-24">短信推送</span><a-switch />
+            <a-input placeholder="请输入手机号" class="w-180 ml-16" readonly value="13554291081" @click="show = true" />
+          </a-col>
+          <a-col>
+            <span class="mr-24">微信推送</span><a-switch />
+          </a-col>
+        </a-row>
       </ConfigCard>
       <div class="mt-48 flex justify-center">
         <a-button type="primary" ghost class="w-200 rounded-100 text-size-24 mx-24">取消</a-button>
         <a-button type="primary" class="w-200 rounded-100 text-size-24">确定</a-button>
       </div>
     </div>
+    <Keyboard v-model:open="show" @ok="onOk" />
   </div>
 </template>
 <!-- <route lang="yaml">
@@ -75,6 +73,11 @@
 }</route>
 <script setup>
 import ConfigCard from './components/ConfigCard.vue';
+const show = ref(false)
+function onOk (val) {
+  console.log(val, key.value);
+  // form[key.value] = val
+}
 </script>
 
 <style lang="less" scoped>
