ee97e030661a6fecebaa4bc20ad644ea285101b1
WM <531431988@qq.com>
2024-04-05 19:00:08 +0800

    添加适配

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import router from './router'
 import App from "./App.vue";
 import './style.less'
-
+// import 'amfe-flexible'
 import 'uno.css'
 const app = createApp(App)
 
