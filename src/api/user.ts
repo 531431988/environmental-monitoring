e56af538051c249c32b568f4bf705c895e0c31ac
WM <531431988@qq.com>
2024-04-06 14:05:04 +0800

    更新接口

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,13 +4,13 @@ import http from '@/utils'
 // 登录
 export const login = data => http({
   url: '/monitor/admin/login',
-  method: 'post',
+  method: 'POST',
   data
 });
 
 // 修改密码
 export const resetPwd = data => http({
   url: '/monitor/admin/reset',
-  method: 'post',
+  method: 'POST',
   data
 });
