b9d2d856c261b6313a46f091dcfb81ced1dcb549
WM <531431988@qq.com>
2024-04-10 11:22:49 +0800

    更新浏览器版本与构建命令

--- src/components.d.ts
+++ src/components.d.ts
@@ -25,8 +25,6 @@ declare module 'vue' {
     ARadioButton: typeof import('ant-design-vue/es')['RadioButton']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
     ARow: typeof import('ant-design-vue/es')['Row']
-    ASelect: typeof import('ant-design-vue/es')['Select']
-    ASelectOption: typeof import('ant-design-vue/es')['SelectOption']
     ASwitch: typeof import('ant-design-vue/es')['Switch']
     Bg: typeof import('./components/Bg.vue')['default']
     Card: typeof import('./components/Card.vue')['default']
