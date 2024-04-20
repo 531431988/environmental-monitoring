434975e2b5c9ffdf2ad8919360d54b3641a94087
wm <531431988@qq.com>
2024-04-20 15:42:25 +0800

    更新键盘不能输入问题

--- src/pages/warning-config/index.vue
+++ src/pages/warning-config/index.vue
@@ -3,40 +3,40 @@
     <ConfigCard title="一级报警处置方案" class="!pb-24">
       <a-row :gutter="16" class="flex items-center justify-center mt-24">
         <a-col>
-          <span class="mr-24">保持供电</span><a-switch v-model:checked="levelOne.electricOn" />
+          <span class="mr-24">保持供电</span><a-switch v-model:checked="form.levelOne.electricOn" />
         </a-col>
         <a-col>
-          <span class="mr-24">报警指示灯</span><a-switch v-model:checked="levelOne.lightOn" />
+          <span class="mr-24">报警指示灯</span><a-switch v-model:checked="form.levelOne.lightOn" />
         </a-col>
         <a-col class="flex items-center">
-          <span class="mr-24">短信推送</span><a-switch v-model:checked="levelOne.pushSms" />
-          <div v-if="levelOne.pushSms" @click="onClick('levelOne')" class="readonly-input">
+          <span class="mr-24">短信推送</span><a-switch v-model:checked="form.levelOne.pushSms" />
+          <div v-if="form.levelOne.pushSms" @click="onClick('phone1')" class="readonly-input">
             <a-input type="text" placeholder="请输入手机号" class="w-150 ml-16" readonly :maxlength="11"
-              v-model:value="levelOne.receivers" />
+              v-model:value="form.phone1" />
           </div>
         </a-col>
         <a-col>
-          <span class="mr-24">微信推送</span><a-switch v-model:checked="levelOne.pushWechat" />
+          <span class="mr-24">微信推送</span><a-switch v-model:checked="form.levelOne.pushWechat" />
         </a-col>
       </a-row>
     </ConfigCard>
     <ConfigCard title="二级报警处置方案" class="my-24 !pb-24">
       <a-row :gutter="16" class="flex items-center justify-center mt-24">
         <a-col>
-          <span class="mr-24">保持供电</span><a-switch v-model:checked="levelTwo.electricOn" />
+          <span class="mr-24">保持供电</span><a-switch v-model:checked="form.levelTwo.electricOn" />
         </a-col>
         <a-col>
-          <span class="mr-24">报警指示灯</span><a-switch v-model:checked="levelTwo.lightOn" />
+          <span class="mr-24">报警指示灯</span><a-switch v-model:checked="form.levelTwo.lightOn" />
         </a-col>
         <a-col class="flex items-center">
-          <span class="mr-24">短信推送</span><a-switch v-model:checked="levelTwo.pushSms" />
-          <div v-if="levelTwo.pushSms" @click="onClick('levelTwo')" class="readonly-input">
+          <span class="mr-24">短信推送</span><a-switch v-model:checked="form.levelTwo.pushSms" />
+          <div v-if="form.levelTwo.pushSms" @click="onClick('phone2')" class="readonly-input">
             <a-input type="text" placeholder="请输入手机号" class="w-150 ml-16" readonly :maxlength="11"
-              v-model:value="levelTwo.receivers" />
+              v-model:value="form.phone2" />
           </div>
         </a-col>
         <a-col>
-          <span class="mr-24">微信推送</span><a-switch v-model:checked="levelTwo.pushWechat" />
+          <span class="mr-24">微信推送</span><a-switch v-model:checked="form.levelTwo.pushWechat" />
         </a-col>
       </a-row>
     </ConfigCard>
@@ -44,7 +44,7 @@
       <a-button type="primary" class="w-200 rounded-100 text-size-24" @click="onSave">保存</a-button>
     </div>
   </div>
-  <Keyboard v-model:open="show" :maxLength="11" @ok="onOk" />
+  <Keyboard v-model:open="show" v-model:value="form[key]" :maxLength="11" @ok="onOk" />
 </template>
 <!-- <route lang="yaml">
   meta:
@@ -62,14 +62,20 @@ import ConfigCard from './components/ConfigCard.vue';
 import * as api from '@/api/warning-config'
 import { message } from 'ant-design-vue';
 const show = ref(false)
-const levelOne = ref({})
-const levelTwo = ref({})
+const form = reactive({
+  levelOne: {},
+  levelTwo: {},
+  phone1: '',
+  phone2: '',
+})
 const key = ref('')
 async function loadConfig () {
   try {
     const { data = [] } = await api.detail()
-    levelOne.value = data[0]
-    levelTwo.value = data[1]
+    form.levelOne = data[0]
+    form.levelTwo = data[1]
+    form.phone1 = data[0].receivers
+    form.phone2 = data[1].receivers
   } catch (error) {
   }
 }
@@ -83,13 +89,22 @@ function onClick (name) {
 }
 
 function onOk (val) {
-  if (key.value === 'levelOne') levelOne.value.receivers = val
-  if (key.value === 'levelTwo') levelTwo.value.receivers = val
+  form[key.value] = val
 }
 
 async function onSave () {
   try {
-    const { data, code } = await api.edit([toRaw(levelOne.value), toRaw(levelTwo.value)])
+    const params  = [
+      {
+        ...toRaw(form.levelOne),
+        receivers: form.phone1
+      },
+      {
+        ...toRaw(form.levelTwo),
+        receivers: form.phone2
+      }
+    ]
+    const { data, code } = await api.edit(params)
     if (code === 200) {
       message.success('保存成功')
       loadConfig()
